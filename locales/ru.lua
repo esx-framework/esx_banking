c9cc31f2d001b2e463c5cd55edbc28bf6b71af53
FPS_86 <alexsaynsk13@mail.ru>
2023-03-16 12:21:26 +0800

    Adding the Russian language

---
+++ locales/ru.lua
@@ -0,0 +1,16 @@
+Locales['ru'] = {
+  ['invalid_amount'] = 'Это недопустимая сумма денег',
+  ['deposit_money']  = 'вы внесли на счет $%s',
+  ['withdraw_money'] = 'вы сняли $%s',
+  ['pincode_money'] = 'у вас новый PIN-код %s',
+  ['transfer_money'] = 'вы успешно перевели деньги $%s ID: %s',
+  ['receive_transfer'] = 'вы получили перевод денег $%s ID: %s',
+  ['press_e_banking']    = 'нажмите [E] чтобы получить доступ к банку',
+  ['access_bank']    = 'Доступ к банку',
+  ['banking_blip']       = 'Банк',
+  ['cant_do_it']       = "Не могу этого сделать!",
+  ['not_enough_money'] = "Недостаточно денег! Вам нужно %s денег!",
+  ['pincode_not_found'] = "Неверный PIN-код",
+  ['pincode_found'] = "Действительный PIN-код...",
+  ['bank_name'] = "Банк Fleeca"
+}
